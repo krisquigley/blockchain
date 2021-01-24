78b67c8e50edce1772b71d910dba50c8ce099eb8
Kris Quigley <kris@krisquigley.co.uk>
2021-01-24 20:48:18 +0000

    Added node-blockchain

---
+++ node-blockchain/dist/bc101.js
@@ -0,0 +1,55 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto = __importStar(require("crypto"));
+class Block {
+    constructor(index, previousHash, timestamp, data) {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = this.calculateHash();
+    }
+    calculateHash() {
+        const data = this.index + this.previousHash + this.timestamp + this.data;
+        return crypto.createHash('sha256').update(data).digest('hex');
+    }
+}
+class Blockchain {
+    constructor() {
+        this.chain = [];
+        this.chain.push(new Block(0, '0', Date.now(), 'Genesis block'));
+    }
+    get latestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(data) {
+        const block = new Block(this.latestBlock.index + 1, this.latestBlock.hash, Date.now(), data);
+        this.chain.push(block);
+    }
+}
+console.log('Creating the blockchain with the genesis block...');
+const blockchain = new Blockchain();
+console.log('Mining block #1...');
+blockchain.addBlock('First block');
+console.log('Mining block #2...');
+blockchain.addBlock('Second block');
+console.log(JSON.stringify(blockchain, null, 2));
